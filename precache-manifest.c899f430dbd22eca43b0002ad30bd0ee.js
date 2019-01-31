ba15c8f41b79dc1455ff852da112c2e3cabb3930
erick <efmoreira@inf.ufpel.edu.br>
2019-01-31 14:40:21 -0200

    Updates

---
+++ precache-manifest.c899f430dbd22eca43b0002ad30bd0ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "85186d8a5ecc4d394375",
+    "url": "/alura_react/static/js/runtime~main.85186d8a.js"
+  },
+  {
+    "revision": "b0fcff7fbd70299b3de2",
+    "url": "/alura_react/static/js/main.b0fcff7f.chunk.js"
+  },
+  {
+    "revision": "a30e861bcb8121bdefb1",
+    "url": "/alura_react/static/js/1.a30e861b.chunk.js"
+  },
+  {
+    "revision": "b0fcff7fbd70299b3de2",
+    "url": "/alura_react/static/css/main.559c97fa.chunk.css"
+  },
+  {
+    "revision": "8bf53cfdb17627e965a3d7b6f61bdec0",
+    "url": "/alura_react/index.html"
+  }
+];

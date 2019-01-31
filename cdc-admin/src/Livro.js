68afb5b00027cbf3bff11c3c15e5defa08c1b297
erick <efmoreira@inf.ufpel.edu.br>
2019-01-31 13:22:18 -0200

    Aula 6(routes)

---
+++ cdc-admin/src/Livro.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class LivroBox extends Component {
+    constructor() {
+        super();
+        this.autorId = 0;
+    }
+
+    render(){
+        return (
+            <div></div>
+        );
+    }
+}
--- cdc-admin/src/index.js
+++ cdc-admin/src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import * as serviceWorker from './serviceWorker';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import AutorBox from './Autor';
+import LivroBox from './Livro';
+import Home from './Home';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    (
+        <Router>
+            <App>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/autor" component={AutorBox} />
+                    <Route path="/livro" component={LivroBox} />
+                </Switch>
+            </App>
+        </Router>
+    ),
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

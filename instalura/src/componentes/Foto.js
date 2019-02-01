d073c7fbe611bdb279d80933c2237cda1f36fb6f
erick <efmoreira@inf.ufpel.edu.br>
2019-02-01 15:02:01 -0200

    Terminando segunda parte do curso (meio mais ou menos .-.

--- instalura/src/componentes/Foto.js
+++ instalura/src/componentes/Foto.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router';
+import { Link } from 'react-router';
 
 class FotoAtualizacoes extends Component {
+
+  like(event) {
+    event.preventDefault();
+    this.props.like(this.props.foto.id);
+  }
+
+  comenta(event) {
+    event.preventDefault();
+    this.props.comenta(this.props.foto.id, this.comentario.value);
+  }
+
   render() {
     return (
       <section className="fotoAtualizacoes">
-        <a href="#" className="fotoAtualizacoes-like">Likar</a>
-        <form className="fotoAtualizacoes-form">
-          <input type="text" placeholder="Adicione um comentário..." className="fotoAtualizacoes-form-campo" />
+        <a onClick={this.like.bind(this)} className={this.props.foto.likeada ? 'fotoAtualizacoes-like-ativo' : 'fotoAtualizacoes-like'}>Likar</a>
+        <form className="fotoAtualizacoes-form" onSubmit={this.comenta.bind(this)}>
+          <input type="text" placeholder="Adicione um comentário..." className="fotoAtualizacoes-form-campo" ref={input => this.comentario = input} />
           <input type="submit" value="Comentar!" className="fotoAtualizacoes-form-submit" />
         </form>
 
@@ -27,7 +38,7 @@ class FotoInfo extends Component {
             })
           }
           curtiram
-              </div>
+        </div>
 
         <p className="foto-info-legenda">
           <a className="foto-info-autor">autor </a>
@@ -76,7 +87,7 @@ export default class FotoItem extends Component {
         <FotoHeader foto={this.props.foto} />
         <img alt="foto" className="foto-src" src={this.props.foto.urlFoto} />
         <FotoInfo foto={this.props.foto} />
-        <FotoAtualizacoes />
+        <FotoAtualizacoes {...this.props} />
       </div>
     );
   }
